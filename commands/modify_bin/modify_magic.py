a95c1297c2c9617e758401ad671043a30ad22642
RTrovallet_QC <romain.trovallet@quasar-concept.fr>
2024-04-15 10:38:16 +0200

    better naming

--- commands/modify_bin/modify_magic.py
+++ commands/modify_bin/modify_magic.py
@@ -1,9 +1,8 @@
 import json
 import os
+import bin_utils as bu
 
 from os import path as p
-
-import bin_utils as b
 
 pyscripts_path = p.dirname(p.dirname(p.dirname(p.realpath(__file__))))
 git_free_path = p.join(pyscripts_path, "git-free")
@@ -15,25 +14,25 @@ def save_to_json(data, filename):
 
 
 def analyze_binary(image_path, json_path):
-    contents = b.open_binary(image_path)
-    output_data = b.process_image(contents)
+    contents = bu.open_binary(image_path)
+    output_data = bu.process_image(contents)
     save_to_json(output_data, json_path)
 
 
 def modify_magic_binary(image_path, new_image_path, new_magic):
     # Read the original binary contents
-    contents = b.open_binary(image_path)
+    contents = bu.open_binary(image_path)
 
     # Unpack the image header to extract the fields
     magic, load_addr, hdr_size, img_size, flags, \
-        ver_major, ver_minor, ver_revision, ver_build_num = b.unpack_image_header(contents)
+        ver_major, ver_minor, ver_revision, ver_build_num = bu.unpack_image_header(contents)
 
     # Pack the modified image header with the new magic number
-    modified_image_header = b.ImageHeader.pack(new_magic, load_addr, hdr_size, img_size, flags,
+    modified_image_header = bu.ImageHeader.pack(new_magic, load_addr, hdr_size, img_size, flags,
                                              ver_major, ver_minor, ver_revision, ver_build_num)
 
     # Combine the modified header with the rest of the binary contents
-    modified_contents = modified_image_header + contents[b.ImageHeader.size:]
+    modified_contents = modified_image_header + contents[bu.ImageHeader.size:]
 
     # Write the modified contents to a new binary file
     with open(new_image_path, 'wb') as f:
